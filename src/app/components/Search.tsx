f88330f0833c5ee1223c216b33cfce7ea0db2102
Meeth Patel <meeth2060@gmail.com>
2025-06-20 11:39:33 +0530

    fuck

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,13 +1,39 @@
 'use client'
+import { products } from "../data/products"
 import { useState } from "react"
+import { item } from "../types/type"
+import itemCard from "./Card"
 export default function Search(){
     const [searchN,setSeatchN] = useState("")
+    const [searchList,setSearchList] = useState<item[]>([])
 
+  function handleSubmit(){
+    const List:item[] = products.filter((product:item)=> {
+               product.name.toLowerCase().includes(searchN.toLowerCase());
+    } )
+    setSearchList(List);
+  }
+    
+    
 
     return(
         <div>
+            <form onSubmit={handleSubmit} >
                <input type="text" placeholder="search" value={searchN} onChange={(e)=>setSeatchN(e.target.value)} />
-            
+               <button type="submit"> Search</button>
+               </form>
+            <div>
+                {searchList && (
+                    searchList.map((value,index)=>{
+                        return(
+                            <div key={index}>
+                                {itemCard(value)}
+                            </div>
+                        )
+                    })
+                    
+                )}
+            </div>
         </div>
     )
 }
