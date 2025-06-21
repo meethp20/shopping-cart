6c13ba09075f1cc5d63116631c4faf93a1a16a6c
Meeth Patel <meeth2060@gmail.com>
2025-06-21 12:25:16 +0530

    hansika

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,10 @@
 
 export default function Navbar(){
 
+ return (
+    <div className="flex justify-around items-center">
+        <h1>S&<span className="text-red-500">K</span></h1>
+        <button className="bg-red-500 rounded-t-2xl font-bold text-white">Login</button>
+    </div>
+ )
 }
